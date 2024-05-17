f85cb0a0e7dd8fe95086e4421ae8eb97d42dad63
Kartik Mehra <167309066+kkartik-k@users.noreply.github.com>
2024-05-17 18:32:30 +0000

    datatypes and variables

---
+++ learnjava/datatypes.js
@@ -0,0 +1,13 @@
+let number = 1234
+const namee = "kartik"
+let isLoggedIn = true
+
+// number -> upto 2 power 53
+// BigInt
+// Boolean -> t/f
+// null -> standalone value 
+// Symbol -> unique values
+// undefined -> not defined
+
+console.log(number)
+console.table([number, namee, isLoggedIn ])

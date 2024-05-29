b3c00e70dfee75d21808e97f988d9b4d7f6803a6
Kartik Mehra <167309066+kkartik-k@users.noreply.github.com>
2024-05-29 11:59:45 +0000

    datatype conversin

---
+++ learnjava/datatype_conversion.js
@@ -0,0 +1,31 @@
+let score = "34abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abs" => Nan
+//true => 1, false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true, 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 69
+
+console.log(someNumber);
+console.log(typeof someNumber);
+
+let changeIt = String(someNumber)
+
+console.log(changeIt);
+console.log(typeof changeIt);

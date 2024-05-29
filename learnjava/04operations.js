75315fccbd338d7dada6c5dba406793a80a62536
Kartik Mehra <167309066+kkartik-k@users.noreply.github.com>
2024-05-29 12:33:14 +0000

    operations and some changes

---
+++ learnjava/04operations.js
@@ -0,0 +1,36 @@
+let value = 3
+
+let negValue = -value
+console.log(negValue);
+
+//*********** operators******
+
+// console.log(12 + 3)
+// console.log(12 - 3)
+// console.log(12 * 3)
+// console.log(12 / 3)
+// console.log(12 % 3)
+
+let str1 = "kartik"
+let str2 = "mehra"
+console.log(str1 + " "+ str2)
+
+let str3 = str1 + str2
+console.log(str3)
+
+// $$$$$$$increment$$$$$
+//post fix
+
+let x = 3;
+const y = x++;
+
+console.log(x , y)
+// x is 4; y is 3
+
+//prefix
+
+let x2 = 3n;
+const y2 = x2++;
+
+console.log(x2, y2)
+// x2 is 4n; y2 is 3n
